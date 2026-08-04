d07293f17ed53e6e2ac65c716d472e11c0e72a7c
Jami Passi <jamipassi.jp@gmail.com>
2026-08-05 00:58:16 +0300

    admin

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product, Order, Coupon, ImportedOrder
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Coupon)
+admin.site.register(ImportedOrder)
